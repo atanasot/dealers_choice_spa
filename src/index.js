9b40e5e3c1c462b6dbe6863aab771bc4fd6b8e79
Lina Ryerkerk <lina.ryerkerk@gmail.com>
2022-02-22 09:22:51 -0500

    added id to li

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 const ul = document.querySelector('ul')
-const render = () => {
-    console.log('hello')
+ul.addEventListener('click', (e) => {
+    const id = e.target.getAttribute('data-id')
+    if(e.target.tagName === 'LI') {
+        console.log(id)
+    }
+})
+const render = async() => {
+    try {
+        const response = await axios.get('/api/todos') //parses a JSON and returns a JS object
+        const todos = response.data //the data is an array of objs
+        console.log(todos) 
+        const html = todos.map(todo => `
+            <li data-id=${todo.id}>${todo.name}</li>
+        `).join('')
+        ul.innerHTML = html
+    } catch (err) {
+        console.log(err)
+    }
 }
 render()
